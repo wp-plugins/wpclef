fb096e941d8c53cd39d2342abc3d293bf67709e0
csixty4 <csixty4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-22 03:25:33 +0000

    v1.2
    git-svn-id: https://plugins.svn.wordpress.org/wpclef/tags/1.2@656649 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ login_script.tpl.php
@@ -0,0 +1,3 @@
+<div style="margin-bottom: 2em;">
+	<script src='https://clef.io/v2/clef.js' class='clef-button' data-app-id='<?php echo $app_id; ?>' data-redirect-url='<?php echo $redirect_url; ?>'></script>
+</div>
